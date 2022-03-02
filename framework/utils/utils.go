61f81daecafce824f66d44734536918157e5f622
Antonio Thomacelli <tonnytg@gmail.com>
2022-03-01 21:17:50 -0300

    feat: add utils to test valid json

---
+++ framework/utils/utils.go
@@ -0,0 +1,14 @@
+package utils
+
+import "encoding/json"
+
+// IsJson returns error or nil if the string is a valid json
+func IsJson(s string) error {
+	var js struct {}
+
+	if err := json.Unmarshal([]byte(s), &js); err != nil {
+		return err
+	}
+
+	return nil
+}
