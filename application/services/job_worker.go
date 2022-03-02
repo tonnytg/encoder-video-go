78aace3ae730639ed5493c7f07590911348886bc
Antonio Thomacelli <tonnytg@gmail.com>
2022-03-02 03:13:10 -0300

    feat: add worker to call jobs

---
+++ application/services/job_worker.go
@@ -0,0 +1,90 @@
+package services
+
+import (
+	"encoding/json"
+	"github.com/google/uuid"
+	"github.com/streadway/amqp"
+	"github.com/tonnytg/encoder-video-go/domain"
+	"github.com/tonnytg/encoder-video-go/framework/utils"
+	"os"
+	"time"
+)
+
+type JobWorkerResult struct {
+	Job     domain.Job
+	Message *amqp.Delivery
+	Error   error
+}
+
+func JobWorker(messageChannel chan amqp.Delivery, returnChan chan JobWorkerResult, jobService JobService, job domain.Job, workerID int) {
+
+	//	{
+	//		"resource_id": "1",
+	//		"file_path": "file.mp4"
+	//	}
+
+	// loop for each message on channel
+	for message := range messageChannel {
+
+		err := utils.IsJson(string(message.Body))
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		// parse json to struct
+		err = json.Unmarshal(message.Body, &jobService.VideoService.Video)
+		jobService.VideoService.Video.ID = uuid.New().String()
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		// Validate the video
+		err = jobService.VideoService.Video.Validate()
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		// Save Video to database
+		err = jobService.VideoService.InsertVideo()
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		job.Video = jobService.VideoService.Video
+		job.OutputBucketPath = os.Getenv("outputBucketName")
+		job.ID = uuid.New().String()
+		job.Status = "STARTING"
+		job.CreateAt = time.Now()
+
+		_, err = jobService.JobRepository.Insert(&job)
+
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		// jobService.Start() control status of job
+		jobService.Job = &job
+		err = jobService.Start()
+
+		if err != nil {
+			returnChan <- returnJobResult(domain.Job{}, message, err)
+			continue
+		}
+
+		returnChan <- returnJobResult(job, message, nil)
+	}
+}
+
+func returnJobResult(job domain.Job, message amqp.Delivery, err error) JobWorkerResult {
+	result := JobWorkerResult{
+		Job:     job,
+		Message: &message,
+		Error:   err,
+	}
+	return result
+}
