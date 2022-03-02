12b861ee35441058b0f38810fb49e59fd055d0e2
Antonio Thomacelli <tonnytg@gmail.com>
2022-03-02 03:12:17 -0300

    test: test json validation

--- framework/utils/utils.go
+++ framework/utils/utils.go
@@ -4,7 +4,7 @@ import "encoding/json"
 
 // IsJson returns error or nil if the string is a valid json
 func IsJson(s string) error {
-	var js struct {}
+	var js struct{}
 
 	if err := json.Unmarshal([]byte(s), &js); err != nil {
 		return err
--- framework/utils/utils_test.go
+++ framework/utils/utils_test.go
@@ -1,1 +1,20 @@
-package utils
+package utils_test
+
+import (
+	"github.com/tonnytg/encoder-video-go/framework/utils"
+	"testing"
+)
+
+func TestIsJson(t *testing.T) {
+	json := `{"a":1}`
+
+	err := utils.IsJson(json)
+	if err != nil {
+		t.Errorf("Correct Json syntax failed: %s", err)
+	}
+
+	err = utils.IsJson(`{"a":1`)
+	if err == nil {
+		t.Errorf("Wrong Json syntax failed: %s", err)
+	}
+}
